e61f2451054045e4694ac6c02d39d10ec487b5fc
pauromeropau <pauromeropau@gmail.com>
2019-09-27 10:00:17 +0200

    remote bd

--- configs/dbconnection.js
+++ configs/dbconnection.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(`mongodb://localhost/${process.env.DB}`, {
+  .connect(process.env.MONGO, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
